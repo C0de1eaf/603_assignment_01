c9744b70fffa756e64242b25836b9dfe31a9e123
C0de1eaf <103360505+C0de1eaf@users.noreply.github.com>
2023-04-25 13:00:31 +1200

    u

--- 603_assignment_01/src/WhoWantsToBeAMillionaire/Game.java
+++ 603_assignment_01/src/WhoWantsToBeAMillionaire/Game.java
@@ -66,7 +66,7 @@ public class Game {
                             System.out.println("Invalid input. Please try again.");
                         } else if (userAnswer == 7) {
                             // fiftyFifty
-                            current = FiftyFifty(current);
+                            
                             current.printQuestion();
                         } else if (userAnswer == 8) {
                             // ask the audience
