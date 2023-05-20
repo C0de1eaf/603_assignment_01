4fe92aaa23897b028e6f5a99eb104fe2da799708
Fumihiro1 <105392494+Fumihiro1@users.noreply.github.com>
2023-05-20 18:02:46 +1200

    Embedded Database
    Yipee

---
+++ WhoWantsToBeAMillionaire/src/WhoWantsToBeAMillionaire/GUI_Database.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WhoWantsToBeAMillionaire;
+
+/**
+ *
+ * @author yutas
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GUI_Database {
+
+    private static final String USER_NAME = "pdc";
+    private static final String PASSWORD = "pdc";
+    private static final String URL = "jdbc:derby:WWTBAM_EmbDB;create=true";
+    private Connection conn;
+
+    public static void main(String[] args) {
+        GUI_Database db = new GUI_Database();
+        db.createConnection();
+    }
+
+    public GUI_Database() {
+        this.createConnection();
+    }
+
+    public void createConnection() {
+        if (this.conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+                System.out.println(URL + " connection successful.");
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
