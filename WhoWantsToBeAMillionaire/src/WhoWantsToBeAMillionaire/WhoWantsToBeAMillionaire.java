80e605b4c3499921d8004c70534be5baa1722ae5
C0de1eaf <103360505+C0de1eaf@users.noreply.github.com>
2023-05-22 15:42:59 +1200

    text box

--- WhoWantsToBeAMillionaire/src/WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire.java
+++ WhoWantsToBeAMillionaire/src/WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire.java
@@ -1,11 +1,22 @@
 package WhoWantsToBeAMillionaire;
 
 import java.io.IOException;
+import javax.swing.JFrame;
 
 public class WhoWantsToBeAMillionaire {
 
     public static void main(String[] args) throws IOException {
-        Game game = new Game();
-        game.runGame();
+        // TODO code application logic here
+        JFrame frame = new JFrame("Moving Balls");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GameGUI GUI = new GameGUI();
+        
+        frame.getContentPane().add(GUI);
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
+        
+        
+//        Game game = new Game();
+//        game.runGame();
     }
 }
