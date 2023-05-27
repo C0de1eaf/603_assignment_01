e4091c5c035aaf766bc2a06a9682535fbf7f6370
C0de1eaf <103360505+C0de1eaf@users.noreply.github.com>
2023-05-28 05:45:48 +1200

    leaderboard return icon (needs fix)

--- WhoWantsToBeAMillionaire/src/WhoWantsToBeAMillionaire/LeaderboardGUI.java
+++ WhoWantsToBeAMillionaire/src/WhoWantsToBeAMillionaire/LeaderboardGUI.java
@@ -3,6 +3,10 @@ package WhoWantsToBeAMillionaire;
 import javax.swing.*;
 import java.awt.*;
 
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+
 public class LeaderboardGUI extends JPanel {
 
     private JButton returnButton;
@@ -13,12 +17,27 @@ public class LeaderboardGUI extends JPanel {
         // Create a return button
         returnButton = new JButton("Return");
 
-        // Add ActionListener to return button
-        returnButton.addActionListener(e -> {
-            cardLayout.show(cards, "menuGUI");
-        });
+        // Set the icon for the return button
+        try {
+            Image img = ImageIO.read(getClass().getResource("resources/return.png"));
+            returnButton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
 
-        // Add return button to the LeaderboardGUI panel
-        add(returnButton, BorderLayout.NORTH);
+        // Remove spacing between the image and button's borders
+        returnButton.setMargin(new Insets(0, 0, 0, 0));
+
+        // Remove border
+        returnButton.setBorderPainted(false);
+
+        // Create a panel with a fixed size
+        JPanel fixedSizePanel = new JPanel();
+        fixedSizePanel.setPreferredSize(new Dimension(50, 50));
+        fixedSizePanel.setLayout(new GridBagLayout());
+        fixedSizePanel.add(returnButton);
+
+        // Add the fixed size panel to the LeaderboardGUI panel
+        add(fixedSizePanel, BorderLayout.NORTH);
     }
 }
