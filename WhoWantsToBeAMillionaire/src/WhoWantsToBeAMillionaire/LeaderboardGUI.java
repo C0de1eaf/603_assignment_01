4fcf11ae4b6df809679216dbee1f452e6ebc7e1d
C0de1eaf <103360505+C0de1eaf@users.noreply.github.com>
2023-05-28 17:11:43 +1200

    BUTTON FIXED :LFG: :LFG: :LFG: :LFG: :LFG:

--- WhoWantsToBeAMillionaire/src/WhoWantsToBeAMillionaire/LeaderboardGUI.java
+++ WhoWantsToBeAMillionaire/src/WhoWantsToBeAMillionaire/LeaderboardGUI.java
@@ -2,6 +2,9 @@ package WhoWantsToBeAMillionaire;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
@@ -13,33 +16,50 @@ public class LeaderboardGUI extends JPanel {
         // Set the layout to FlowLayout with left alignment
         setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        // Create a return button
-        returnButton = new JButton();
-        returnButton.setPreferredSize(new Dimension(100, 100));
-
         // Set the icon for the return button
         try {
-            Image img = ImageIO.read(getClass().getResource("resources/return.png"));
-            returnButton.setIcon(new ImageIcon(img));
+            JPanel returnPanel = new JPanel();
+            returnPanel.setBounds(50, 50, 250, 250);
+
+            BufferedImage panelImage = ImageIO.read(new File("resources/return.png"));
+            ImageIcon returnButtonIcon = new ImageIcon(panelImage);
+
+            JButton returnButton = new JButton(returnButtonIcon);
+            // Set the background color of the return button to solid light grey
+            returnButton.setBackground(new Color(192, 192, 192)); // RGB: 192, 192, 192 (light grey)
+
+            // Remove border
+            returnButton.setBorderPainted(false);
+
+            // Add ActionListener to return button
+            returnButton.addActionListener(e -> {
+                cardLayout.show(cards, "menuGUI");
+            });
+
+            // Remove spacing between the image and button's borders
+            returnButton.setMargin(new Insets(0, 0, 0, 0));
+
+            returnPanel.add(returnButton);
+            add(returnPanel);
+
         } catch (Exception ex) {
             System.out.println(ex);
         }
 
-        // Set the background color of the return button to solid light grey
-        returnButton.setBackground(new Color(192, 192, 192)); // RGB: 192, 192, 192 (light grey)
-
-        // Remove spacing between the image and button's borders
-        returnButton.setMargin(new Insets(0, 0, 0, 0));
-
-        // Remove border
-        returnButton.setBorderPainted(false);
-
-        // Add ActionListener to return button
-        returnButton.addActionListener(e -> {
-            cardLayout.show(cards, "menuGUI");
-        });
-
-        // Add the return button to the LeaderboardGUI panel
-        add(returnButton);
+//        // Set the background color of the return button to solid light grey
+//        returnButton.setBackground(new Color(192, 192, 192)); // RGB: 192, 192, 192 (light grey)
+//
+//        // Remove spacing between the image and button's borders
+//        returnButton.setMargin(new Insets(0, 0, 0, 0));
+//
+//        // Remove border
+//        returnButton.setBorderPainted(false);
+//
+//        // Add ActionListener to return button
+//        returnButton.addActionListener(e -> {
+//            cardLayout.show(cards, "menuGUI");
+//        });
+//
+//        add(returnButton);
     }
 }
